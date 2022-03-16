20701b1cd21f4473d3669007a927b44ad7511a7f
Joao Aquino <whisoer@users.noreply.github.com>
2022-03-16 11:12:06 -0300

    Create new route to create client stream

--- server/routes.js
+++ server/routes.js
@@ -14,51 +14,65 @@ async function routes(request, response) {
 
   if (method === "GET" && url === "/") {
     response.writeHead(302, {
-      'Location': location.home,
+      Location: location.home,
     });
 
-    return response.end()
+    return response.end();
   }
 
   if (method === "GET" && url === "/home") {
     const { stream } = await controller.getFileStream(homeHTML);
-    return stream.pipe(response)
+    return stream.pipe(response);
   }
 
   if (method === "GET" && url === "/controller") {
     const { stream } = await controller.getFileStream(controllerHTML);
-    return stream.pipe(response)
+    return stream.pipe(response);
   }
 
-  if (method === 'GET') {
+  if (method === "GET" && url.includes("/stream")) {
+    const { stream, onClose } = controller.createClientStream();
+    request.once("close", onClose);
+    response.writeHead(200, {
+      "Content-Type": "audio/mpeg",
+      "Accept-Rages": "bytes",
+    });
+
+    return stream.pipe(response);
+  }
+
+  //files
+  if (method === "GET") {
     const { stream, type } = await controller.getFileStream(url);
     const contentType = CONTENT_TYPE[type];
 
     if (contentType) {
       response.writeHead(200, {
-        'Content-Type': contentType,
-      })
+        "Content-Type": contentType,
+      });
     }
 
-    return stream.pipe(response)
+    return stream.pipe(response);
   }
 
-  response.writeHead(404)
+  response.writeHead(404);
   return response.end("hello");
 }
 
 function handleError(error, response) {
-  if (error.message.includes('ENOENT')) {
-    logger.warn(`Asset not found ${error.stack}`)
-    response.writeHead(404)
-    return response.end()
+  if (error.message.includes("ENOENT")) {
+    logger.warn(`Asset not found ${error.stack}`);
+    response.writeHead(404);
+    return response.end();
   }
 
-  logger.error(`caught error on API: ${error.stack}`)
-  response.writeHead(500)
-  return response.end()
+  logger.error(`caught error on API: ${error.stack}`);
+  response.writeHead(500);
+  return response.end();
 }
 
 export function handler(request, response) {
-  return routes(request, response).catch(error => handleError(error, response));
+  return routes(request, response).catch((error) =>
+    handleError(error, response)
+  );
 }
