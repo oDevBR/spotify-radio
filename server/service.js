aaec49111fe77067c6a1f65b42a7e49d2aa00e23
Joao Aquino <whisoer@users.noreply.github.com>
2022-03-16 11:11:26 -0300

    Create service to create client stream

--- server/service.js
+++ server/service.js
@@ -1,32 +1,51 @@
+import { randomUUID } from "crypto";
 import fs from "fs";
-import fsPromises from 'fs/promises'
+import fsPromises from "fs/promises";
+import { extname, join } from "path";
+import { PassThrough } from "stream";
 
-import { join, extname } from "path";
 import config from "./config.js";
+
 const {
   dir: { publicDirectory },
 } = config;
 
 export class Service {
+  constructor() {
+    this.clientStreams = new Map();
+  }
+
+  createClientStream() {
+    const id = randomUUID();
+    const clientStream = new PassThrough()
+    this.clientStreams.set(id, clientStream)
+
+    return { id, clientStream }
+  }
+
+  removeClientStream(id) {
+    this.clientStreams.delete(id)
+  }
+
   createFileStream(filename) {
     return fs.createReadStream(filename);
   }
 
   async getFileInfo(file) {
-    const fullFilePath = join(publicDirectory, file)
-    await fsPromises.access(fullFilePath)
-    const fileType = extname(fullFilePath)
+    const fullFilePath = join(publicDirectory, file);
+    await fsPromises.access(fullFilePath);
+    const fileType = extname(fullFilePath);
     return {
       type: fileType,
-      name: fullFilePath
-    }
+      name: fullFilePath,
+    };
   }
 
   async getFileStream(file) {
     const { name, type } = await this.getFileInfo(file);
     return {
       stream: this.createFileStream(name),
-      type
+      type,
     };
   }
 }
