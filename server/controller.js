638b51501c5c9ed5e2a01136b8e707fb6e72e3f2
Joao Aquino <whisoer@users.noreply.github.com>
2022-03-16 22:01:07 -0300

    Add a handle command to controller

--- server/controller.js
+++ server/controller.js
@@ -1,26 +1,44 @@
 import { Service } from "./service.js";
-import { logger } from './util.js';
+import { logger } from "./util.js";
 
 export class Controller {
   constructor() {
-    this.service = new Service()
+    this.service = new Service();
   }
 
   async getFileStream(filename) {
-    return this.service.getFileStream(filename)
+    return this.service.getFileStream(filename);
+  }
+
+  async handleCommand({ command }) {
+    logger.info(`command receive: ${command}`);
+    const result = {
+      result: "ok",
+    };
+
+    const cmd = command.toLowerCase();
+    if (cmd.includes("start")) {
+      this.service.startStreaming();
+      return result;
+    }
+    if (cmd.includes("stop")) {
+      this.service.stopStreaming();
+      return result;
+    }
+    return this.service.startStreaming();
   }
 
   createClientStream() {
-    const { id, clientStream } = this.service.createClientStream()
+    const { id, clientStream } = this.service.createClientStream();
 
     const onClose = () => {
-      logger.info(`closing connection of ${id}`)
-      this.service.removeClientStream(id)
-    }
+      logger.info(`closing connection of ${id}`);
+      this.service.removeClientStream(id);
+    };
 
     return {
       stream: clientStream,
-      onClose
-    }
+      onClose,
+    };
   }
 }
